2e612c0711a09c04d0ff20138a1ac247398c96a8
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-02-17 12:22:11 -0500

    Partial functionallity home screen aurora

---
+++ src/components/background/AppBackgroundComponent.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="h-screen w-screen bg-[#0E9577] relative overflow-hidden z-0">
+    <div
+      class="app-texture z-5 opacity-10 w-full h-full absolute top-0 left-0 cover"
+    />
+    <div class="absolute blur-[250px] bg-opacity-80 app-1" />
+    <div class="absolute blur-[250px] bg-opacity-80 app-2" />
+    <div class="absolute blur-[2500px] bg-opacity-80 app-3" />
+    <div class="absolute blur-[250px] bg-opacity-80 app-4" />
+    <div class="absolute blur-[250px] bg-opacity-80 app-5" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBackgroundComponent",
+};
+</script>
+
+<style scoped lang="scss">
+@keyframes fly {
+  100% {
+    transform: rotate(1turn) translate(100px) rotate(-1turn);
+  }
+}
+
+@keyframes flyPlus {
+  100% {
+    transform: rotate(-1turn) translate(100px) rotate(1turn);
+  }
+}
+
+.app-1 {
+  border-radius: 100%;
+  width: 900px;
+  height: 400px;
+  background-color: rgba(4, 222, 173, 0.5);
+  left: -50px;
+  top: -300px;
+  z-index: 3;
+  animation: flyPlus 12s linear infinite;
+  transform: rotate(0) translate(150px) rotate(0);
+}
+
+.app-2 {
+  border-radius: 100%;
+  width: 900px;
+  height: 800px;
+  animation: flyPlus 8s linear infinite;
+  transform: rotate(0) translate(100px) rotate(0);
+  background-color: rgba(14, 149, 119, 1);
+  bottom: -30px;
+  left: -80px;
+}
+
+.app-3 {
+  border-radius: 100%;
+  width: 450px;
+  height: 450px;
+  bottom: -80px;
+  right: -100px;
+  background-color: rgba(4, 222, 173, 0.7);
+  animation: flyPlus 8s linear infinite;
+  transform: rotate(0) translate(100px) rotate(0);
+}
+
+.app-4 {
+  background-color: rgba(14, 149, 119, 1);
+  top: -30px;
+  right: -80px;
+  width: 900px;
+  height: 800px;
+  animation: flyPlus 2s linear infinite;
+  transform: rotate(0) translate(100px) rotate(0);
+}
+
+.app-5 {
+  border-radius: 100%;
+  top: 0;
+  right: 400px;
+  width: 900px;
+  height: 900px;
+  background-color: rgba(241, 239, 185, 0.78);
+  transform: rotate(0) translate(250px) rotate(0);
+  animation: flyPlus 7s linear infinite;
+}
+
+.app-texture {
+  background-image: url("@/assets/images/bg-texture.png");
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import AppBackgroundComponent from "@/components/background/AppBackgroundComponent.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="relative">
+    <section class="xl:absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+      <h1
+        class="stretch text-6xl font-bold text-center w-full text-neutral-900"
+      >
+        THIS IS YOUR WAY <br />
+        TO ALL WHAT YOU <br />
+        NEED
+      </h1>
+      <router-link
+        to="/portfolio"
+        class="poppins font-semibold text-white uppercase border border-transparent ease-in-out duration-200 hover:border-b-white focus:text-neutral-900 foucs:border-b-neutral-900 mt-10"
+      >
+        Portfolio
+      </router-link>
+    </section>
+    <app-background-component />
   </main>
 </template>
+
+<style scoped>
+</style>
